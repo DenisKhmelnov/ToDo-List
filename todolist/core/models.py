ddc79d6b4d80a4b0e23eb591e4dee1235593a2a2
Denis Khmelnov <denishmelnov@gmail.com>
2023-02-07 22:03:59 +0300

    feat: implement custom admin panell

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    REQUIRED_FIELDS = []
+
     class Meta:
-        verbose_name = "Пользователи"
-        verbose_name_plural = "Пользователь"
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
--- todolist/settings.py
+++ todolist/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist.core'
+    'todolist.core',
 ]
 
 MIDDLEWARE = [
@@ -51,22 +51,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'todolist.urls'
 
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [BASE_DIR / 'templates']
-#         ,
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'todolist.wsgi.application'
 
